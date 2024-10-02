a5fcdef044f15a0679353ea1bb7acb8dcc303621
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-10-02 23:09:55 +0530

    css module

--- src/components/components/product-items.jsx
+++ src/components/components/product-items.jsx
@@ -1,5 +1,7 @@
+import styles from "./product-item.module.css" //this provides an object with the style names and the values that can be used here
 function ButtonComponent(){
-    return <button>Click</button>;
+    console.log(styles.buttonStyle);
+    return <button className={styles.buttonStyle}>Click</button>;
 }
 
 function ProductItem({singleProductItem, key}) {
